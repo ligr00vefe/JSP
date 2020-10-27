d5c8fa1d236d73b4bf5609d2913d362e1a93525b
ligr00vefe <ligr00vefe@naver.com>
2020-10-27 22:40:16 +0900

    2020/09/13 JSP 수업 필터

---
+++ Ch19/src/service/RegisterService.java
@@ -0,0 +1,35 @@
+package service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RegisterService implements CommonService {
+
+	@Override
+	public String requestProc(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		if(req.getMethod().equals("POST")) {
+			
+			String name = req.getParameter("name");
+			String addr = req.getParameter("addr");
+			String hp = req.getParameter("hp");
+			
+			req.setCharacterEncoding("utf-8");
+			req.setAttribute("name", name);
+			req.setAttribute("addr", addr);
+			req.setAttribute("hp", hp);
+			
+			return "/result.jsp";
+			
+		}else {
+			
+			return "/register.jsp";
+			
+		}
+		
+	}
+
+}
